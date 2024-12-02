3626de5f279c8f208a555057bf28e0f3c2bf2611
Tony <tomcatbuzz@yahoo.com>
2024-12-01 23:51:23 -0500

    iw canvas, working. Check build for main.ts

--- src/app/canvas-case/canvas-case.component.ts
+++ src/app/canvas-case/canvas-case.component.ts
@@ -15,8 +15,15 @@ private scene!: THREE.Scene;
 private camera!: THREE.PerspectiveCamera;
 private cubes: THREE.Mesh[] = [];
 private clock = new THREE.Clock();
+private raycaster = new THREE.Raycaster();
 private mouse = new THREE.Vector2();
 private basePositions: THREE.Vector3[] = [];
+private colorCycle = [
+  new THREE.Color(0x3498db),  // Blue
+  new THREE.Color(0xe74c3c),  // Red
+  new THREE.Color(0x2ecc71)   // Green
+];
+private currentColorIndex = 0;
 // private hoverStrength = 0;
 // private hoverPosition = new THREE.Vector3();
 // private hoverTimeout: any;
@@ -83,6 +90,7 @@ createCubes() {
     uniform float mouseStrength;
     varying vec3 vPosition;
     varying float vDistanceToMouse;
+    uniform float positionOffset;
 
     void main() {
       vPosition = position;
@@ -97,7 +105,7 @@ createCubes() {
 
       // Mouse effect
       float mouseEffect = smoothstep(1.0, 0.0, distanceToMouse / 3.0);
-      transformed.z += mouseEffect * mouseStrength * 0.5;
+      transformed.z += mouseEffect * mouseStrength * positionOffset;
 
       // vPosition = transformed
       gl_Position = projectionMatrix * modelViewMatrix * vec4(transformed, 1.0);
@@ -108,12 +116,14 @@ createCubes() {
     uniform float mouseStrength;
     varying vec3 vPosition;
     varying float vDistanceToMouse;
+    uniform vec3 customColor;
     
     void main() {
-      vec3 baseColor = vec3(0.5, 0.7, 0.9);
+      // vec3 baseColor = vec3(0.5, 0.7, 0.9);
+      
       float colorIntensity = 1.0 + (mouseStrength * 0.3);
 
-      vec3 finalColor = baseColor * colorIntensity;
+      vec3 finalColor = customColor * colorIntensity;
       gl_FragColor = vec4(finalColor, 1.0);
     }
   `;
@@ -129,7 +139,9 @@ createCubes() {
     uniforms: {
       time: { value: 0 },
       mousePosition: { value: new THREE.Vector3() },
-      mouseStrength: { value: 0 }
+      mouseStrength: { value: 0 },
+      customColor: { value: this.colorCycle[0] },
+      positionOffset: { value: 0.5 }
     }
   });
 
@@ -248,22 +260,47 @@ onMouseMove(event: MouseEvent) {
   this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-  const raycaster = new THREE.Raycaster();
-  raycaster.setFromCamera(this.mouse, this.camera);
-  const intersects = raycaster.intersectObjects(this.cubes);
+  
+  this.raycaster.setFromCamera(this.mouse, this.camera);
+  const intersects = this.raycaster.intersectObjects(this.cubes);
   // console.log(intersects, "mouse over")
 
-  // Reset interaction
-  this.cubes.forEach((cube, index) => {
-    const material = cube.material as THREE.ShaderMaterial;
-    material.uniforms['mouseStrength'].value = 0;
+  this.currentColorIndex = (this.currentColorIndex + 1) % this.colorCycle.length;
+  const nextColor = this.colorCycle[this.currentColorIndex];
 
-    gsap.to(cube.position, {
-      z: this.basePositions[index].z, 
+  // Reset interaction Original, maybe broken?
+  // this.cubes.forEach((cube, index) => {
+  //   const material = cube.material as THREE.ShaderMaterial;
+  //   material.uniforms['mouseStrength'].value = 0;
+
+  //   gsap.to(cube.position, {
+  //     z: this.basePositions[index].z, 
+  //     duration: 0.5,
+  //     ease: 'power2.out'
+  //   })
+  // });
+
+  // reset cubes
+  this.cubes.forEach(cube => {
+    const material = cube.material as THREE.ShaderMaterial;
+    
+    gsap.to(material.uniforms['mouseStrength'], {
+      value: 0,
+      duration: 0.3,
+      // onComplete: () => {
+      //   material.uniforms['customColor'].value.copy(nextColor);
+      // }
+    });
+
+    // smooth color transition
+    gsap.to(material.uniforms['customColor'].value, {
+      r: nextColor.r,
+      g: nextColor.g,
+      b: nextColor.b,
       duration: 0.5,
-      ease: 'power2.out'
-    })
-  });
+      // ease here????
+    });
+  })
 
   if (intersects.length > 0) {
     intersects.forEach(intersect => {
@@ -286,7 +323,7 @@ onMouseMove(event: MouseEvent) {
       });
 
       gsap.to(cube.position, {
-        z: cube.position.z + 0.1,
+        z: cube.position.z + 0.5,
         duration: 0.3,
         ease: 'power2.out'
       })
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -27,15 +27,27 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular({ navAnimation: pageTransition }),
     provideRouter(routes), 
     provideAnimations(),
-    importProvidersFrom(
-    provideFirebaseApp(() => initializeApp({"projectId":"ionicwebpage","appId":"1:952994598736:web:c051e724ce521f59cca655","databaseURL":"https://ionicwebpage.firebaseio.com","storageBucket":"ionicwebpage.appspot.com","apiKey":"AIzaSyBytj8gvFINALswEUnSwtUBBRoDfUuQDJw","authDomain":"ionicwebpage.firebaseapp.com","messagingSenderId":"952994598736"})), 
-    provideAnalytics(() => getAnalytics()), ScreenTrackingService, 
+
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      projectId: "ionicwebpage",
+      appId: "1:952994598736:web:c051e724ce521f59cca655",
+      databaseURL: "https://ionicwebpage.firebaseio.com",
+      storageBucket: "ionicwebpage.appspot.com",
+      apiKey: "AIzaSyBytj8gvFINALswEUnSwtUBBRoDfUuQDJw",
+      authDomain: "ionicwebpage.firebaseapp.com",
+      messagingSenderId: "952994598736"
+    })), 
+
+    provideAnalytics(() => getAnalytics()), 
+    ScreenTrackingService, 
     provideDatabase(() => getDatabase()), 
     provideFunctions(() => getFunctions()), 
     provideStorage(() => getStorage())),
-    importProvidersFrom(RecaptchaV3Module), 
+
+    importProvidersFrom(RecaptchaV3Module, HttpClientModule), 
+    // provideHttpClient(withInterceptorsFromDi()),
     
     { provide: RECAPTCHA_V3_SITE_KEY, useValue: environment.recaptcha.siteKey },
-    importProvidersFrom(HttpClientModule)
-  ],
+    
+  ]
 });
